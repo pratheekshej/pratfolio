9d710c30c64d9b8d9e9f42bd797d0fcab6000e78
Pratheeksh Joseph <pratheekshej@gmail.com>
2024-03-03 20:45:50 -0500

    UI updates in Hero section.

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 const styles = {
     paddingX: "sm:px-16 px-6",
-    contentPaddingX: "sm:px-48 px-16",
+    contentPaddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
 
